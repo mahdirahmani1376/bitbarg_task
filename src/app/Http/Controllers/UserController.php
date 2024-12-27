947453cf4e08a906366151e66002bdf891371b23
acer <rahmanimahdi16@gmail.com>
2024-12-27 10:59:06 +0330

    add spatie roles package

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -3,15 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Actions\User\RespondWithTokenAction;
-use App\Models\User;
-use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Auth;
 use App\Actions\User\StoreUserAction;
 use App\Actions\User\UserLoginAction;
 use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\StoreUserRequest;
-use Illuminate\Auth\AuthenticationException;
 
 class UserController extends Controller
 {
